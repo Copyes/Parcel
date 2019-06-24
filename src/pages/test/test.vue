c507630917c6806b9da4fda91d86fb7c0a4ec7bf
Copyes <fan_545697167@qq.com>
2019-06-24 23:24:05 +0800

    :sparkles: new project

---
+++ src/pages/test/test.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="test">
+    sss
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'test'
+  }
+</script>
+
+<style>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
