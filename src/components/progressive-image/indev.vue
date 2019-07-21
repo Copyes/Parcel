687b1ab84f1731d0c13455577b00c8f77617b646
Copyes <fan_545697167@qq.com>
2019-07-21 21:40:05 +0800

    :sparkles: new component added

---
+++ src/components/progressive-image/indev.vue
@@ -0,0 +1,25 @@
+<template>
+  <div ref="image" :class="['progressive-image', customClass]" :style="componentStyle">
+    <div 
+      v-if="cached"
+      class="progressive-image-wrapper"
+      :style="wrapperStyle"
+      >
+        <img 
+          :class="['progressive-image-content', imageCustomClass]"
+          :src="image" :alt="alt" />
+    </div>
+    <div v-else>
+      <canvas></canvas>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+
+</style>
