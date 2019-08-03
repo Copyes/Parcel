a4b639c51071b3786eb10f9c38e85f67f0dee5d8
Copyes <fan_545697167@qq.com>
2019-08-03 21:36:20 +0800

    add

--- src/components/progressive-image/indev.vue
+++ src/components/progressive-image/indev.vue
@@ -10,7 +10,35 @@
           :src="image" :alt="alt" />
     </div>
     <div v-else>
-      <canvas></canvas>
+      <canvas
+        v-if="!isShowImage"
+        width="1"
+        height="1"
+        class="progressive-image-canvas"
+        ref="canvas"
+      >
+      </canvas>
+      <div class="progressive-image-wrapper" :style="wrapperStyle">
+        <transition
+        enter-class="progressive-image-enter"
+        enter-active-class="progressive-image-before-enter">
+          <img 
+          v-show="isShowImage"
+          :class="['progressive-image-content', imageCustomClass]"
+          ref="content"
+          :src="image" :alt="alt">
+        </transition>
+        <transition
+          enter-class="progressive-image-enter"
+          enter-active-class="progressive-image-before-enter">
+          <div
+            v-if="isShowPlaceholder"
+            class="progressive-image-placeholder"
+            :class="{ 'progressive-image-placeholder-out': isShowImage }"
+            :style="placeholderStyle">
+          </div>
+        </transition>
+      </div>
     </div>
   </div>
 </template>
